bfc3992445c91e06023b8784d284d3e8801ab9e0
asgaraliyev <asgaraliyev2001@gmail.com>
2022-05-30 21:30:01 +0400

    rebuilding from scratch

--- pages/components/BoldLine.tsx
+++
@@ -1,7 +0,0 @@
-import { useColorMode, useColorModePreference, useColorModeValue } from "@chakra-ui/react";
-import React from "react";
-
-export default function BoldLine() {
-  const bg = useColorModeValue("bg-slate-900","bg-slate-400")
-  return <section className={`w-full h-5 ${bg}`}></section>;
-}
